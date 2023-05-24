40a3bb3aa1a1478c15431e2fd29290c8effa80b2
Jon Jackson <ocskier@gmail.com>
2023-05-23 22:32:50 -0400

    Adds some types

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,17 @@
 import { HTMLAttributes } from 'react';
 
+import './Button.style.scss';
+
 export interface IButtonProps extends HTMLAttributes<HTMLButtonElement> {
   text: string;
 }
 
 const Button = ({ text, ...rest }: IButtonProps) => {
-  return <button {...rest}>{text}</button>;
+  return (
+    <button className="custom-btn" {...rest}>
+      {text}
+    </button>
+  );
 };
 
 export default Button;
