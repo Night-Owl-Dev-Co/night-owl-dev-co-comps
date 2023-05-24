1936f67e47dd65b2460af809a743ac92962721bb
Jon Jackson <ocskier@gmail.com>
2023-05-24 00:12:46 -0400

    Adds memo with theme obj

--- src/components/ThemeProvider/ThemeProvider.tsx
+++ src/components/ThemeProvider/ThemeProvider.tsx
@@ -1,11 +1,28 @@
 import {
   createContext,
   useContext,
-  useEffect,
+  useMemo,
   useState,
   FC,
   PropsWithChildren,
 } from 'react';
+
+const themes: { [key: string]: { [key: string]: string } } = {
+  default: {
+    primaryColor: '#FFA72A',
+    accentColor: '#458622',
+    backgroundColor: '#FFC777',
+    textColor: '#504f4d',
+    secondaryColor: '#7F5315',
+  },
+  dark: {
+    primaryColor: '#FFA72A',
+    accentColor: '#458622',
+    backgroundColor: '#504f4d',
+    textColor: '#FFC777',
+    secondaryColor: '#252525',
+  },
+};
 
 const ThemeContext = createContext({});
 
@@ -18,8 +35,10 @@ export interface IThemeProviderProps extends PropsWithChildren {
 const ThemeProvider: FC<IThemeProviderProps> = ({ children }) => {
   const [theme, withTheme] = useState('default');
 
+  const ThemeStyle = useMemo(() => themes[theme], [theme]);
+
   return (
-    <ThemeContext.Provider value={{ theme, withTheme }}>
+    <ThemeContext.Provider value={{ ThemeStyle, withTheme }}>
       {children}
     </ThemeContext.Provider>
   );
